fa3bb341a55a46cfc45b5a026a1d3344c7ced275
Shelyn Del Mundo <dm.shelyn@gmail.com>
2024-03-26 14:05:15 -0600

    authentication error update

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AuthContextProvider } from "./components/_utils/auth-context";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthContextProvider>{children}</AuthContextProvider>
+      </body>
     </html>
   );
 }
+ 
+// const Layout = ({ children }) => {
+//   return <AuthContextProvider>{children}</AuthContextProvider>;
+// };
+ 
+// export default Layout;
